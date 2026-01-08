4ecb7ed950ccccafd8aa1b0114896c1cb4fe91d3
Viktor <157784316+ADmex1@users.noreply.github.com>
2026-01-08 22:30:55 +0800

    SuperAdminSeeder Version 1

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Container\Attributes\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
+use Illuminate\Support\Str;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $uuid = Str::uuid()->toString();
+        $admin = User::factory()->create([
+            "name" => "useruser3",
+            'email' => "useruser3@gmail.com",
+            "password" => bcrypt("shqiptare"),
+            'profile_qr' => $uuid
+            
+        ]);
+        $admin->assignRole('super-admin');
+    
+        $qrPath = "qrcodes/users/{$uuid}.svg";
+        FacadesStorage::disk('public')->put(
+            $qrPath,
+            QrCode::format('svg')
+            ->size(200)
+            ->generate("USER:{$uuid}")
+        );
+        $admin->update([
+            'qr_code_path'=>$qrPath
+        ]);
+    }
+}
