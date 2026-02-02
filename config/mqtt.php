02da90fc7a621403b6e8d71e8f98ef8e957947e7
dekyudi99 <ikadekyudiartana602@gmail.com>
2026-02-02 13:26:13 +0800

    add weights from mqtt hivemq

---
+++ config/mqtt.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => env('MQTT_HOST'),
+    'port' => env('MQTT_PORT'),
+    'username' => env('MQTT_USERNAME'),
+    'password' => env('MQTT_PASSWORD'),
+    'client_id' => env('MQTT_CLIENT_ID'),
+];
